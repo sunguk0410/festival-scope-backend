c1f5da3c332f8fb5959bb28eb4fd196876660e4a
kkcc4104 <kkcc4104@naver.com>
2026-09-18 17:42:59 +0900

    fix: CORS 허용 설정

--- src/main/java/likelion/festivalscope/global/config/SecurityConfig.java
+++ src/main/java/likelion/festivalscope/global/config/SecurityConfig.java
@@ -24,7 +24,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://www.festival-scope.co.kr"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
